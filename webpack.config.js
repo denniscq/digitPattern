58c5cf5f9c337242ae31040656d280236b061be8
Chen <qiang.c.chen@accenture.com>
2023-04-07 19:45:40 +0800

    init repo

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Development',
+        }),
+      ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    optimization: {
+        runtimeChunk: 'single'
+    }
+};
